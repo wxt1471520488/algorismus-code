be73d87fe1b9d4a9cd3e15ccc8b175b0b28053a9
wangxt <wangxt@ckt.cn>
2022-07-26 09:45:00 +0800

    init

---
+++ src/main/java/com/wangxt/algorismus/code/AlgorismusCodeApplication.java
@@ -0,0 +1,13 @@
+package com.wangxt.algorismus.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorismusCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorismusCodeApplication.class, args);
+	}
+
+}
